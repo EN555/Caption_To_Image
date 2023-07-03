09e0160e9884eb317ded818b409e34846299d9ca
EN555 <eviatarn@gmail.com>
2023-07-03 13:19:08 +0300

    fix bug

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 from torchvision import transforms
+from transformers import AutoTokenizer
+import torch
 
 resize_transform = transforms.Resize((299, 299))    # for the pretrain inception module
 
@@ -9,33 +11,45 @@ class encodeImage(nn.Module):
     """
     transfer learning
     """
-    def __init__(self, embedding_size):
+    def __init__(self, embedding_size=50):
         super(encodeImage, self).__init__()
-        self.inception = models.inception_v3(pretrained=True, aux_logits=False)
+        self.inception = models.inception_v3(pretrained=True, aux_logits=True)
         self.inception.requires_grad_(False)
         self.linear = nn.Linear(self.inception.fc.in_features, embedding_size)
-        self.relu = F.relu()
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
         x = resize_transform(x)
-        x = self.dropout(self.relu(self.inception(x)))
+        x = self.dropout(self.inception(x))
         x = self.linear(x)
         return x
 
 
 class decodeImage(nn.Module):
-    def __init__(self, tokenizer):
+    def __init__(self, vocab_len):
         super(decodeImage, self).__init__()
-        self.embedding = nn.Embedding(len(tokenizer), 100)
+        self.embedding = nn.Embedding(vocab_len, 100)
         self.lstm = nn.LSTM(100, 50)
-        self.relu = F.relu()
         self.dropout = nn.Dropout(0.5)
 
-    def forward(self, x):
+    def forward(self, x, caption):
         x = self.embedding(x)
-        lstm = self.dropout(self.relu(self.lstm(x)))
+        x = torch.cat(x, caption)
+        lstm = self.dropout(self.lstm(x))
+        return lstm
 
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, vocab_len):
+        super(EncoderDecoder, self).__init__()
+        self.encdoer = encodeImage()
+        self.decoder = decodeImage(vocab_len)
+
+    def forward(self, x, caption):
+        encoding = self.encdoer(x)
+        decoding = self.decoder(encoding, caption)
+        return decoding
 
 if __name__ == '__main__':
-    encode_img = encodeImage(50)
+    tokenizer = AutoTokenizer.from_pretrained("roberta-large")
+    enc_dec = EncoderDecoder(len(tokenizer))
