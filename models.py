e6c1df7cab0543f9cb1927392026afda70f6aa55
EN555 <eviatarn@gmail.com>
2023-07-03 12:57:01 +0300

    WIP - models

--- models.py
+++ models.py
@@ -1,15 +1,41 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
+from torchvision import transforms
 
+resize_transform = transforms.Resize((299, 299))    # for the pretrain inception module
 
 class encodeImage(nn.Module):
-    def __init__(self, embedded_size):
-        conv1 = F.conv2d()
-        max_pool = F.max_pool2d()
-        self.dropout = F.dropout()
+    """
+    transfer learning
+    """
+    def __init__(self, embedding_size):
+        super(encodeImage, self).__init__()
+        self.inception = models.inception_v3(pretrained=True, aux_logits=False)
+        self.inception.requires_grad_(False)
+        self.linear = nn.Linear(self.inception.fc.in_features, embedding_size)
+        self.relu = F.relu()
+        self.dropout = nn.Dropout(0.5)
 
-
+    def forward(self, x):
+        x = resize_transform(x)
+        x = self.dropout(self.relu(self.inception(x)))
+        x = self.linear(x)
+        return x
 
 
 class decodeImage(nn.Module):
-    pass
+    def __init__(self, tokenizer):
+        super(decodeImage, self).__init__()
+        self.embedding = nn.Embedding(len(tokenizer), 100)
+        self.lstm = nn.LSTM(100, 50)
+        self.relu = F.relu()
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        lstm = self.dropout(self.relu(self.lstm(x)))
+
+
+if __name__ == '__main__':
+    encode_img = encodeImage(50)
--- train.py
+++ train.py
@@ -1,7 +1,15 @@
+import torch
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+from get_loader import imageCaptionsDataset
+from models import *
 
 
-def train():
-    pass
+def train(batch_size=32):
+    tokenizer = AutoTokenizer.from_pretrained("roberta-large")
+    dataset = imageCaptionsDataset(tokenizer)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=)
+    enc_img = encodeImage()
 
 
 
