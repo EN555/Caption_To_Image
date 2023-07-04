c73b7ec576b80d972bddd841805f5e20242b6c70
EN555 <eviatarn@gmail.com>
2023-07-04 11:58:25 +0300

    almost done the recursive method

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class encodeImage(nn.Module):
     """
     transfer learning
     """
-    def __init__(self, embedding_size=50):
+    def __init__(self, embedding_size=100):
         super(encodeImage, self).__init__()
         self.inception = models.inception_v3(pretrained=True, aux_logits=True)
         self.inception.requires_grad_(False)
@@ -30,28 +30,53 @@ class decodeImage(nn.Module):
         super(decodeImage, self).__init__()
         self.embedding = nn.Embedding(vocab_len, 100)
         self.lstm = nn.LSTM(100, 50, batch_first=True)
+        self.linear = nn.Linear(50, vocab_len)
         self.dropout = nn.Dropout(0.5)
 
-    def forward(self, encoding, caption):
-        caption = self.embedding(caption)
-        caption = torch.transpose(caption, 0, 1)
-        x = caption.unsqueeze(1).expand(caption.size(0), encoding.size(1), caption.size(1))
-        x = torch.cat(encoding, x)
-        x = x.reshape(caption.size(0), caption.size(1),-1)
-        lstm = self.dropout(self.lstm(x))
-        return lstm
+    def forward(self, input, hidden, caption=None, img=0):
+        """
+        :param input:
+        :param hidden:
+        :param caption:
+        :param img: bool parameter, check if to use nn.embedding
+        :return:
+        """
+        # teacher forcing
+        if not img:
+            input = self.embedding(input)
+        if caption:
+            caption = self.embedding(caption)
+            caption = torch.transpose(caption, 0, 1)
+            input = torch.cat((input.unsqueeze(1), caption), dim=1)
+        output, hidden = self.lstm(input, hidden)
+        output = self.dropout(self.linear(output))
+        return output, hidden
 
 
 class EncoderDecoder(nn.Module):
-    def __init__(self, vocab_len):
+    def __init__(self, tokenizer):
         super(EncoderDecoder, self).__init__()
         self.encdoer = encodeImage()
-        self.decoder = decodeImage(vocab_len)
+        self.decoder = decodeImage(len(tokenizer))
+        self.tokenizer = tokenizer
 
     def forward(self, x, caption, len_cap):
         encoding = self.encdoer(x)
         decoding = self.decoder(encoding, caption)
         return decoding
+
+
+    def recursive_method(self, input, padding_idx, max_len=30):
+        hidden = None
+        input = self.encdoer(input)
+        img = 1
+        for idx, _ in enumerate(range(max_len)):
+            output, hidden = self.decoder(input=input, hidden=hidden, img=0 if idx == 0 else 1)
+            img = 0
+            softmax = torch.softmax(output, dim=1)
+            output = input
+
+
 
 if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained("roberta-large")
--- train.py
+++ train.py
@@ -6,33 +6,44 @@ from models import EncoderDecoder
 from torch.nn.utils.rnn import pad_sequence
 import torch.nn as nn
 import torch.optim as optim
+from torchvision import transforms
 
 
 
+resize_transform = transforms.Resize((299, 299))    # for the pretrain inception module
 
 def collate_fn(data, tokenizer):
     image, caption = zip(*data)  # need to return too the attentions mask
     caption_lengths = [len(i) for i in caption]
     pad_token_id = tokenizer.pad_token_id
     caption = pad_sequence(caption, padding_value=pad_token_id)
-    return image, caption, caption_lengths
+    return torch.stack([resize_transform(img) for img in image]), caption, caption_lengths
 
 
-def train(batch_size=32):
+def train(batch_size=32, training_prediction="recursive"):
+    """
+    :param batch_size:
+    :param training_prediction: implementation of two methods: recursive and teacher forcing
+    :return:
+    """
     # parameters
     learning_rate = 0.001
     epochs = 2
     tokenizer = AutoTokenizer.from_pretrained("roberta-large")
+    print(len(tokenizer))
     dataset = imageCaptionsDataset(tokenizer)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=lambda x: collate_fn(x, tokenizer))
-    enc_img = EncoderDecoder(len(tokenizer))
+    enc_dec = EncoderDecoder(tokenizer)
     criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
-    optimizer = optim.Adam(enc_img.parameters(), lr=learning_rate)
-    enc_img.train()
+    optimizer = optim.Adam(enc_dec.parameters(), lr=learning_rate)
+    enc_dec.train()
     for epoch in range(epochs):
         for batch in dataloader:
             img, cap, len_cap = batch
-            enc_img(img, cap, len_cap)
+            if training_prediction == "recursive":
+                enc_dec.recursive_method(img, tokenizer.pad_token_id, max_len=30)
+            elif training_prediction == "teacher_forcing":
+                pass
 
 
 
