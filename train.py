9678eafb0259fa8ffe20c91e57a7f7293bde9dc7
EN555 <eviatarn@gmail.com>
2024-01-20 21:54:02 +0200

    change train method

--- train.py
+++ train.py
@@ -1,24 +1,18 @@
 import torch
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
-from get_loader import imageCaptionsDataset
-from models import EncoderDecoder
 from torch.nn.utils.rnn import pad_sequence
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import transforms
+from PIL import Image
+import os
+import torch
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+from get_loader import *
+from models import *
 
-
-
-resize_transform = transforms.Resize((299, 299))    # for the pretrain inception module
-
-def collate_fn(data, tokenizer):
-    image, caption = zip(*data)  # need to return too the attentions mask
-    caption_lengths = [len(i) for i in caption]
-    pad_token_id = tokenizer.pad_token_id
-    caption = pad_sequence(caption, padding_value=pad_token_id)
-    return torch.stack([resize_transform(img) for img in image]), caption, caption_lengths
-
+# def prediction(image, )
 
 def train(batch_size=32, training_prediction="recursive"):
     """
@@ -26,28 +20,28 @@ def train(batch_size=32, training_prediction="recursive"):
     :param training_prediction: implementation of two methods: recursive and teacher forcing
     :return:
     """
-    # parameters
-    learning_rate = 0.001
-    epochs = 2
-    tokenizer = AutoTokenizer.from_pretrained("roberta-large")
-    print(len(tokenizer))
-    dataset = imageCaptionsDataset(tokenizer)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=lambda x: collate_fn(x, tokenizer))
-    enc_dec = EncoderDecoder(tokenizer)
-    criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
-    optimizer = optim.Adam(enc_dec.parameters(), lr=learning_rate)
-    enc_dec.train()
-    for epoch in range(epochs):
-        for batch in dataloader:
-            img, cap, len_cap = batch
-            if training_prediction == "recursive":
-                enc_dec.recursive_method(img, tokenizer.pad_token_id, max_len=30)
-            elif training_prediction == "teacher_forcing":
-                pass
+    batch_size = 32
+    image_captions = parse_captions()
+    dataset = ImageCaptionDataset("data/images", image_captions)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    encoder = ViTEncoder(100)
+    decoder = DecoderT5()
+    optimizer = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=0.001)
+    criterion = nn.CrossEntropyLoss(ignore_index=decoder.tokenizer.pad_token_id)
 
+    for images, captions in dataloader:
+        optimizer.zero_grad()
+        image_features = encoder(images)
 
+        captions_input = [" ".join(caption.split(" ")[:-1]) for caption in captions]  # Inputs exclude the last token
+        captions_target = [" ".join(caption.split(" ")[1:]) for caption in captions]  # Targets exclude the first token
 
+        logits = decoder(image_features, captions_input)
+        loss = criterion(logits.view(-1, logits.size(-1)), captions_target.contiguous().view(-1))
+        loss.backward()
+        optimizer.step()
 
+        print(f"Training Loss: {loss.item()}")
 
 
 if __name__ == '__main__':
